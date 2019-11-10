9a9f06ab6e955abdac8f80033b8bf0dc9173eec5
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-11-10 13:12:32 +0100

    camera controling

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,94 @@
 #include <iostream>
+#include <tuple>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-void glfw_error_callback(int error, const char *description)
+std::tuple<glm::mat4, glm::mat4> matrices_from_input(GLFWwindow *window)
 {
-    std::cout << "Error " << error << ": " << description << '\n';
+    static auto last_time = glfwGetTime();
+    const auto current_time = glfwGetTime();
+    const auto delta_time = static_cast<float>(current_time - last_time);
+    last_time = current_time;
+
+    static auto horizontal_angle = 3.14f;
+    static auto vertical_angle = 0.0f;
+    constexpr auto mouse_speed = 0.002f;
+
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
+    static double last_xpos = xpos;
+    static double last_ypos = ypos;
+
+    horizontal_angle += mouse_speed * static_cast<float>(last_xpos - xpos);
+    vertical_angle += mouse_speed * static_cast<float>(last_ypos - ypos);
+    last_xpos = xpos;
+    last_ypos = ypos;
+
+    const glm::vec3 direction(
+        glm::cos(vertical_angle) * glm::sin(horizontal_angle),
+        glm::sin(vertical_angle),
+        glm::cos(vertical_angle) * glm::cos(horizontal_angle));
+
+    const glm::vec3 right = glm::vec3(
+        glm::sin(horizontal_angle - 3.14f / 2.0f),
+        0,
+        glm::cos(horizontal_angle - 3.14f / 2.0f));
+
+    const glm::vec3 up = glm::cross(right, direction);
+
+    static glm::vec3 position = glm::vec3(0.0f, 0.0f, 5.0f);
+    constexpr auto speed = 3.0f;
+
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    {
+        position += direction * delta_time * speed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    {
+        position -= direction * delta_time * speed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+    {
+        position += right * delta_time * speed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+    {
+        position -= right * delta_time * speed;
+    }
+
+    static float fov = 45.0f;
+    constexpr auto fov_speed = 1.05f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        fov /= fov_speed;
+        if (fov < 15.0f)
+            fov = 15.0f;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        fov *= fov_speed;
+        if (fov > 100.0f)
+            fov = 100.0f;
+    }
+
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+    const auto proj = glm::perspective(glm::radians(fov),
+                                       static_cast<float>(width) / static_cast<float>(height),
+                                       0.1f,
+                                       100.0f);
+
+    const auto view = glm::lookAt(
+        position,
+        position + direction,
+        up);
+
+    return std::make_tuple(view, proj);
 }
 
 int main()
@@ -14,13 +97,19 @@ int main()
     if (!glfwInit())
         return -1;
 
-    glfwSetErrorCallback(glfw_error_callback);
+    glfwSetErrorCallback([](int error, const char *description) {
+        std::cout << "Error " << error << ": " << description << '\n';
+    });
+
+    constexpr int window_width = 1280;
+    constexpr int window_height = 960;
 
     GLFWwindow *window;
+    glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    window = glfwCreateWindow(1280, 960, "Hello World", nullptr, nullptr);
+    window = glfwCreateWindow(window_width, window_height, "Hello World", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -31,20 +120,38 @@ int main()
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
     glfwSwapInterval(1);
 
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
+
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_CULL_FACE);
+
     constexpr auto vertex_shader_text = R"(
         #version 330 core
         layout(location = 0) in vec3 vpos;
+        layout(location = 1) in vec3 vcol;
+
+        out vec3 frag_col;
+
+        uniform mat4 MVP;
+
         void main()
         {  
-            gl_Position.xyz = vpos;
-            gl_Position.w = 1.0;
+            gl_Position = MVP * vec4(vpos, 1.0);
+            frag_col = vcol;
         }
         )";
     constexpr auto fragment_shader_text = R"(
         #version 330 core
+        
+        in vec3 frag_col;
+        
         out vec3 color;
+        
         void main(){
-          color = vec3(1,0,0);
+          color = frag_col;
         }
         )";
 
@@ -65,9 +172,80 @@ int main()
     glDeleteShader(fragment_shader);
 
     constexpr float vertices[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f};
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, -1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, -1.0f, 1.0f};
+
+    constexpr float colors[] = {
+        0.583f, 0.771f, 0.014f,
+        0.609f, 0.115f, 0.436f,
+        0.327f, 0.483f, 0.844f,
+        0.822f, 0.569f, 0.201f,
+        0.435f, 0.602f, 0.223f,
+        0.310f, 0.747f, 0.185f,
+        0.597f, 0.770f, 0.761f,
+        0.559f, 0.436f, 0.730f,
+        0.359f, 0.583f, 0.152f,
+        0.483f, 0.596f, 0.789f,
+        0.559f, 0.861f, 0.639f,
+        0.195f, 0.548f, 0.859f,
+        0.014f, 0.184f, 0.576f,
+        0.771f, 0.328f, 0.970f,
+        0.406f, 0.615f, 0.116f,
+        0.676f, 0.977f, 0.133f,
+        0.971f, 0.572f, 0.833f,
+        0.140f, 0.616f, 0.489f,
+        0.997f, 0.513f, 0.064f,
+        0.945f, 0.719f, 0.592f,
+        0.543f, 0.021f, 0.978f,
+        0.279f, 0.317f, 0.505f,
+        0.167f, 0.620f, 0.077f,
+        0.347f, 0.857f, 0.137f,
+        0.055f, 0.953f, 0.042f,
+        0.714f, 0.505f, 0.345f,
+        0.783f, 0.290f, 0.734f,
+        0.722f, 0.645f, 0.174f,
+        0.302f, 0.455f, 0.848f,
+        0.225f, 0.587f, 0.040f,
+        0.517f, 0.713f, 0.338f,
+        0.053f, 0.959f, 0.120f,
+        0.393f, 0.621f, 0.362f,
+        0.673f, 0.211f, 0.457f,
+        0.820f, 0.883f, 0.371f,
+        0.982f, 0.099f, 0.879f};
 
     GLuint vertex_array;
     glGenVertexArrays(1, &vertex_array);
@@ -78,23 +256,47 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    GLuint color_buffer;
+    glGenBuffers(1, &color_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    const auto mvp_id = glGetUniformLocation(program, "MVP");
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(program);
+
+        const auto [view, projection] = matrices_from_input(window);
+        const auto mvp = projection * view * glm::mat4(1.0f);
+
+        glUniformMatrix4fv(mvp_id, 1, GL_FALSE, glm::value_ptr(mvp));
+
         glEnableVertexAttribArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(float) / 3);
+
         glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
 
         glfwSwapBuffers(window);
 
         glfwPollEvents();
+
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            break;
     }
 
     glDeleteBuffers(1, &vertex_buffer);
+    glDeleteBuffers(1, &color_buffer);
     glDeleteVertexArrays(1, &vertex_array);
     glDeleteProgram(program);
 
