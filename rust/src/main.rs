a9aeddcec6f2ecdecad9d9870c334180c2674b06
matszczygiel <mateusz.szczygiel@student.uw.edu.pl>
2019-12-04 17:01:56 +0100

    camera movement fix

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -225,7 +225,7 @@ fn main() {
     let (sphere_va, sphere_vb, sphere_ib) = crate_sphere_buffers(1.0);
 
     let mut cam = Camera::new_default(WIDTH, HEIGTH);
-    const CAM_SPEED: f32 = 0.0001;
+    const CAM_SPEED: f32 = 0.00006;
     const FOV_SPEED: f32 = 1.05;
     const MOUSE_SPEED: f32 = 0.002;
 
@@ -234,117 +234,151 @@ fn main() {
     let mut time = Instant::now();
     let mut delta_t = time.elapsed();
 
+    let mut moving_up = false;
+    let mut moving_down = false;
+    let mut moving_right = false;
+    let mut moving_left = false;
+
     use event::*;
-    el.run(move |event, _, control_flow| match event {
-        Event::EventsCleared => {
-            if !window_focused {
-                windowed_context.window().request_redraw();
-            }
-        }
-        Event::LoopDestroyed => return,
-        Event::WindowEvent { ref event, .. } => match event {
-            WindowEvent::CloseRequested => *control_flow = event_loop::ControlFlow::Exit,
-            WindowEvent::Resized(logical_size) => {
-                let dpi_factor = windowed_context.window().hidpi_factor();
-                windowed_context.resize(logical_size.to_physical(dpi_factor));
-                windowed_context.window().request_redraw();
-            }
-            WindowEvent::Focused(f) => {
-                window_focused = *f;
-                if window_focused {
+    el.run(move |event, _, control_flow| {
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { ref event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = event_loop::ControlFlow::Exit,
+                WindowEvent::Resized(logical_size) => {
+                    let dpi_factor = windowed_context.window().hidpi_factor();
+                    windowed_context.resize(logical_size.to_physical(dpi_factor));
+                }
+                WindowEvent::Focused(f) => {
+                    window_focused = *f;
+                    if window_focused {
+                        windowed_context.window().set_cursor_grab(true).ok();
+                        windowed_context.window().set_cursor_visible(false);
+                    } else {
+                        windowed_context.window().set_cursor_grab(false).ok();
+                        windowed_context.window().set_cursor_visible(true);
+                    }
+                }
+                WindowEvent::MouseInput {
+                    state: ElementState::Pressed,
+                    ..
+                } => {
                     windowed_context.window().set_cursor_grab(true).ok();
                     windowed_context.window().set_cursor_visible(false);
-                } else {
-                    windowed_context.window().set_cursor_grab(false).ok();
-                    windowed_context.window().set_cursor_visible(true);
+                    window_focused = true;
+                    *control_flow = event_loop::ControlFlow::Wait;
                 }
-            }
-            WindowEvent::MouseInput {
-                state: ElementState::Pressed,
-                ..
-            } => {
-                windowed_context.window().set_cursor_grab(true).ok();
-                windowed_context.window().set_cursor_visible(false);
-                window_focused = true;
-                windowed_context.window().request_redraw();
-                *control_flow = event_loop::ControlFlow::Wait;
-            }
-
-            WindowEvent::KeyboardInput { input, .. } if input.state == ElementState::Pressed => {
-                match input.virtual_keycode {
+                WindowEvent::KeyboardInput { input, .. } => match input.virtual_keycode {
                     Some(VirtualKeyCode::Escape) => {
                         *control_flow = event_loop::ControlFlow::Exit;
-                        windowed_context.window().request_redraw();
                     }
                     Some(VirtualKeyCode::Up) => {
-                        cam.position += cam.direction() * delta_t.as_micros() as f32 * CAM_SPEED;
-                        windowed_context.window().request_redraw();
+                        moving_up = match input.state {
+                            ElementState::Pressed => true,
+                            ElementState::Released => false,
+                        };
                     }
                     Some(VirtualKeyCode::Down) => {
-                        cam.position -= cam.direction() * delta_t.as_micros() as f32 * CAM_SPEED;
-                        windowed_context.window().request_redraw();
+                        moving_down = match input.state {
+                            ElementState::Pressed => true,
+                            ElementState::Released => false,
+                        };
                     }
                     Some(VirtualKeyCode::Right) => {
-                        cam.position += cam.right() * delta_t.as_micros() as f32 * CAM_SPEED;
-                        windowed_context.window().request_redraw();
+                        moving_right = match input.state {
+                            ElementState::Pressed => true,
+                            ElementState::Released => false,
+                        };
                     }
                     Some(VirtualKeyCode::Left) => {
-                        cam.position -= cam.right() * delta_t.as_micros() as f32 * CAM_SPEED;
-                        windowed_context.window().request_redraw();
+                        moving_left = match input.state {
+                            ElementState::Pressed => true,
+                            ElementState::Released => false,
+                        };
                     }
                     Some(VirtualKeyCode::W) => {
                         cam.perspective.fovy /= FOV_SPEED;
                         if cam.perspective.fovy < Rad::from(Deg(15.0)) {
                             cam.perspective.fovy = Rad::from(Deg(15.0));
                         }
-                        windowed_context.window().request_redraw();
                     }
                     Some(VirtualKeyCode::S) => {
                         cam.perspective.fovy *= FOV_SPEED;
                         if cam.perspective.fovy > Rad::from(Deg(100.0)) {
                             cam.perspective.fovy = Rad::from(Deg(100.0));
                         }
-                        windowed_context.window().request_redraw();
                     }
                     Some(VirtualKeyCode::R) => {
                         let size = windowed_context.window().inner_size();
                         cam = Camera::new_default(size.width as f32, size.height as f32);
-                        windowed_context.window().request_redraw();
                     }
                     _ => (),
-                }
-            }
-            WindowEvent::RedrawRequested => {
-                unsafe {
-                    gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-                }
-                let (view, projection) = cam.to_vp();
-                sphere_shader.bind();
-                sphere_shader.set_uniform_mat4f("projection", &projection);
-                sphere_shader.set_uniform_mat4f("view", &view);
-                sphere_shader.set_uniform_3f("camPos", &cam.position.to_homogeneous().truncate());
-                sphere_va.bind();
-                sphere_ib.bind();
+                },
+                WindowEvent::RedrawRequested => {
+                    if moving_up {
+                        cam.position += cam.direction() * delta_t.as_micros() as f32 * CAM_SPEED;
+                    }
+                    if moving_down {
+                        cam.position -= cam.direction() * delta_t.as_micros() as f32 * CAM_SPEED;
+                    }
+                    if moving_right {
+                        cam.position += cam.right() * delta_t.as_micros() as f32 * CAM_SPEED;
+                    }
+                    if moving_left {
+                        cam.position -= cam.right() * delta_t.as_micros() as f32 * CAM_SPEED;
+                    }
 
-                const ROWS: i32 = 7;
-                const COLS: i32 = 7;
-                const SPACING: f32 = 2.5;
+                    unsafe {
+                        gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+                    }
+                    let (view, projection) = cam.to_vp();
+                    sphere_shader.bind();
+                    sphere_shader.set_uniform_mat4f("projection", &projection);
+                    sphere_shader.set_uniform_mat4f("view", &view);
+                    sphere_shader
+                        .set_uniform_3f("camPos", &cam.position.to_homogeneous().truncate());
+                    sphere_va.bind();
+                    sphere_ib.bind();
 
-                for row in 0..ROWS {
-                    let metallness = row as f32 / ROWS as f32;
-                    sphere_shader.set_uniform_1f("metallic", &metallness);
-                    for col in 0..COLS {
-                        let roughness = (col as f32 / COLS as f32).max(0.05).min(1.0);
-                        sphere_shader.set_uniform_1f("roughness", &roughness);
+                    const ROWS: i32 = 7;
+                    const COLS: i32 = 7;
+                    const SPACING: f32 = 2.5;
 
-                        let translation = vec3::<f32>(
-                            col as f32 - (COLS as f32 / 2.0),
-                            row as f32 - (ROWS as f32 / 2.0),
-                            0.0,
-                        ) * SPACING;
-                        let model = Matrix4::<f32>::from_translation(translation);
+                    for row in 0..ROWS {
+                        let metallness = row as f32 / ROWS as f32;
+                        sphere_shader.set_uniform_1f("metallic", &metallness);
+                        for col in 0..COLS {
+                            let roughness = (col as f32 / COLS as f32).max(0.05).min(1.0);
+                            sphere_shader.set_uniform_1f("roughness", &roughness);
+
+                            let translation = vec3::<f32>(
+                                col as f32 - (COLS as f32 / 2.0),
+                                row as f32 - (ROWS as f32 / 2.0),
+                                0.0,
+                            ) * SPACING;
+                            let model = Matrix4::<f32>::from_translation(translation);
+                            sphere_shader.set_uniform_mat4f("model", &model);
+
+                            unsafe {
+                                gl::DrawElements(
+                                    gl::TRIANGLE_STRIP,
+                                    sphere_ib.count() as i32,
+                                    gl::UNSIGNED_INT,
+                                    std::ptr::null(),
+                                );
+                            }
+                        }
+                    }
+
+                    for i in 0..light_positions.len() {
+                        sphere_shader
+                            .set_uniform_3f(&format!("lightPositions[{}]", i), &light_positions[i]);
+                        sphere_shader
+                            .set_uniform_3f(&format!("lightColors[{}]", i), &light_colors[i]);
+                        let model = Matrix4::<f32>::from_translation(light_positions[i])
+                            * Matrix4::<f32>::from_scale(0.5);
+
                         sphere_shader.set_uniform_mat4f("model", &model);
-
                         unsafe {
                             gl::DrawElements(
                                 gl::TRIANGLE_STRIP,
@@ -354,64 +388,48 @@ fn main() {
                             );
                         }
                     }
-                }
 
-                for i in 0..light_positions.len() {
-                    sphere_shader
-                        .set_uniform_3f(&format!("lightPositions[{}]", i), &light_positions[i]);
-                    sphere_shader.set_uniform_3f(&format!("lightColors[{}]", i), &light_colors[i]);
-                    let model = Matrix4::<f32>::from_translation(light_positions[i])
-                        * Matrix4::<f32>::from_scale(0.5);
-
-                    sphere_shader.set_uniform_mat4f("model", &model);
+                    let skybox_view = {
+                        let mut v = view.clone();
+                        v[3][0] = 0.0;
+                        v[3][1] = 0.0;
+                        v[3][2] = 0.0;
+                        v[3][3] = 0.0;
+                        v[0][3] = 0.0;
+                        v[1][3] = 0.0;
+                        v[2][3] = 0.0;
+                        v
+                    };
+                    skybox_shader.bind();
+                    skybox_va.bind();
+                    skybox_shader.set_uniform_mat4f("view", &skybox_view);
+                    skybox_shader.set_uniform_mat4f("projection", &projection);
+                    skybox_texture.bind();
+                    skybox_shader.set_texture_slot("skybox", &0);
                     unsafe {
-                        gl::DrawElements(
-                            gl::TRIANGLE_STRIP,
-                            sphere_ib.count() as i32,
-                            gl::UNSIGNED_INT,
-                            std::ptr::null(),
-                        );
+                        gl::DepthFunc(gl::LEQUAL);
+                        gl::DrawArrays(gl::TRIANGLES, 0, 36);
+                        gl::DepthFunc(gl::LESS);
                     }
-                }
+                    windowed_context.swap_buffers().unwrap();
 
-                let skybox_view = {
-                    let mut v = view.clone();
-                    v[3][0] = 0.0;
-                    v[3][1] = 0.0;
-                    v[3][2] = 0.0;
-                    v[3][3] = 0.0;
-                    v[0][3] = 0.0;
-                    v[1][3] = 0.0;
-                    v[2][3] = 0.0;
-                    v
-                };
-                skybox_shader.bind();
-                skybox_va.bind();
-                skybox_shader.set_uniform_mat4f("view", &skybox_view);
-                skybox_shader.set_uniform_mat4f("projection", &projection);
-                skybox_texture.bind();
-                skybox_shader.set_texture_slot("skybox", &0);
-                unsafe {
-                    gl::DepthFunc(gl::LEQUAL);
-                    gl::DrawArrays(gl::TRIANGLES, 0, 36);
-                    gl::DepthFunc(gl::LESS);
+                    delta_t = time.elapsed();
+                    time = Instant::now();
+                    println!("Time: {}ms", delta_t.as_micros() / 1000);
                 }
-                windowed_context.swap_buffers().unwrap();
-
-                delta_t = time.elapsed();
-                time = Instant::now();
-                println!("Time: {}ms", delta_t.as_micros() / 1000);
-            }
+                _ => (),
+            },
+            Event::DeviceEvent { ref event, .. } => match event {
+                DeviceEvent::MouseMotion { delta: (x, y) } => {
+                    cam.horizontal_angle -= Rad(*x as f32 * MOUSE_SPEED);
+                    cam.vertical_angle -= Rad(*y as f32 * MOUSE_SPEED);
+                }
+                _ => (),
+            },
             _ => (),
-        },
-        Event::DeviceEvent { ref event, .. } => match event {
-            DeviceEvent::MouseMotion { delta: (x, y) } => {
-                cam.horizontal_angle -= Rad(*x as f32 * MOUSE_SPEED);
-                cam.vertical_angle -= Rad(*y as f32 * MOUSE_SPEED);
-                windowed_context.window().request_redraw();
-            }
-            _ => (),
-        },
-        _ => (),
+        }
+        if window_focused {
+            windowed_context.window().request_redraw();
+        }
     });
 }
